d0b4185a09e1cc2e3d54453d0e7256e6b7aaf98d
HankelBao <hankelbao@outlook.com>
2017-03-29 15:29:04 +0800

    I have no internet now.... so i send it to my computer, wishing to update it to git.coding after class, there may be something wrong with the git since i lost some git version update

--- handler/suggestionCommit.php
+++ handler/suggestionCommit.php
@@ -8,4 +8,7 @@ $save_xml = $suggestionsXml->asXML();
 $file = fopen("suggestions.xml","w");
 fwrite($file,$save_xml);
 fclose($file);
+
+echo "Thanks for your suggestion!!";
+echo "<a href='../index.php'>GO BACK</a>";
 ?>
